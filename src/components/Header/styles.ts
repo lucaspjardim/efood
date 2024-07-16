d3f2565a155c906353c9d9886e1efdb7b600798a
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-07-16 07:58:11 -0300

    Fix no banner

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,7 +7,7 @@ export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0px 200px;
+  padding: 0px 245px;
   height: 166px;
   font-size: 18px;
   font-weight: 900;
